61559c5c77a1c525b8027daf2b16c34b618b81f6
Hajar Chettih <hajarchettih@gmail.com>
2024-02-03 11:15:14 +0100

    first-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InputButtonUnitComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button-unit',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './input-button-unit.component.html',
+  styleUrls: ['./input-button-unit.component.scss']
+})
+export class InputButtonUnitComponent {
+
+}
